bd0eef666091ca1d4190ad071e86a6df04080917
devops4sure <devops4sure@gmail.com>
2026-07-21 23:38:09 -0700

    new add

--- terraform/modules/monitoring/variables.tf
+++ terraform/modules/monitoring/variables.tf
@@ -3,36 +3,6 @@ variable "namespace" {
   default = "monitoring"
 }
 
-variable "grafana_admin_user" {
-  type      = string
-  sensitive = true
-}
-
-variable "grafana_admin_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "smtp_user" {
-  type      = string
-  sensitive = true
-}
-
-variable "smtp_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "smtp_host" {
-  type = string
-}
-
-variable "smtp_from" {
-  type = string
-}
-
-
-
 variable "grafana_hostname" {
   type = string
 }

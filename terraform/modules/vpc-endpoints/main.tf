73f15aece7366f6120544fbc7fefa40c16a6321f
devops4sure <devops4sure@gmail.com>
2026-08-05 07:25:34 -0700

    fixed missing file

---
+++ terraform/modules/vpc-endpoints/main.tf
@@ -0,0 +1,70 @@
+# S3 Gateway endpoint
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = var.private_route_table_ids
+}
+
+# ECR API Interface endpoint
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.ecr.api"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = var.private_subnet_ids
+  security_group_ids = [var.node_sg_id]
+}
+
+# ECR DKR Interface endpoint
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = var.private_subnet_ids
+  security_group_ids = [var.node_sg_id]
+}
+
+# STS Interface endpoint
+resource "aws_vpc_endpoint" "sts" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.sts"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = var.private_subnet_ids
+  security_group_ids = [var.node_sg_id]
+}
+
+# CloudWatch Logs Interface endpoint
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.logs"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = var.private_subnet_ids
+  security_group_ids = [var.node_sg_id]
+}
+
+# EC2 Interface endpoint
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.ec2"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = var.private_subnet_ids
+  security_group_ids = [var.node_sg_id]
+}
+
+# Autoscaling Interface endpoint
+resource "aws_vpc_endpoint" "autoscaling" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.autoscaling"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = var.private_subnet_ids
+  security_group_ids = [var.node_sg_id]
+}
+
+# KMS Interface endpoint
+resource "aws_vpc_endpoint" "kms" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.kms"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = var.private_subnet_ids
+  security_group_ids = [var.node_sg_id]
+}

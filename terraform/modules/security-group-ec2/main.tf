cd9cc2e7990bd3fcd558d23b482bcee6ff99a542
devops4sure <devops4sure@gmail.com>
2026-08-07 22:38:10 -0700

    latest fix

--- terraform/modules/security-group-ec2/main.tf
+++ terraform/modules/security-group-ec2/main.tf
@@ -36,6 +36,14 @@ resource "aws_security_group" "web" {
     cidr_blocks = [var.vpc_cidr]
   }
 
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
   tags = {
     Name = var.sg_name
   }

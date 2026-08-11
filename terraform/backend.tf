8cd3b029bbaeae35839e7ed12f42f40477b1d78a
devops4sure <devops4sure@gmail.com>
2026-08-11 02:17:21 -0700

    new fix on networking

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "minishop-prod-terraform-state"
     key            = "prod/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform-locks-2"
   }
 }

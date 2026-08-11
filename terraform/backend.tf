2706777c27c1428d4034c229d49dc3210937675a
devops4sure <devops4sure@gmail.com>
2026-08-11 01:54:51 -0700

    new fix on networking

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
-    encrypt        = true
+    use_lockfile = true  
 
   }
 }

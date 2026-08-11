2f3a76e8e12877f77c7ec1c1253c2a4d49bd1f6b
devops4sure <devops4sure@gmail.com>
2026-08-11 01:56:55 -0700

    new fix on networking

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -33,15 +33,7 @@ resource "helm_release" "otel_collector" {
     })
   ]
 
-  set {
-    name  = "image.repository"
-    value = "otel/opentelemetry-collector"
-  }
-
-  set {
-    name  = "image.tag"
-    value = "0.102.0"
-  }
+ 
 
   depends_on = [
     helm_release.promtail

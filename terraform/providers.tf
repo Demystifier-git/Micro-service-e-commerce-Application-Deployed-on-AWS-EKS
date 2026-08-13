5f2189947eb095f74bfde282e1ec1bb5446f6c76
devops4sure <devops4sure@gmail.com>
2026-08-13 00:02:16 -0700

    new fix

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,39 +1,28 @@
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
 
-
-
 provider "kubernetes" {
-  host = module.eks.cluster_endpoint
-
-  cluster_ca_certificate = base64decode(
-    module.eks.cluster_ca_certificate
-  )
-
-  token = data.aws_eks_cluster_auth.this.token
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
   kubernetes {
-    host = module.eks.cluster_endpoint
-
-    cluster_ca_certificate = base64decode(
-      module.eks.cluster_ca_certificate
-    )
-
-    token = data.aws_eks_cluster_auth.this.token
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
   }
 }
 
 provider "kubectl" {
-  host = module.eks.cluster_endpoint
-
-  cluster_ca_certificate = base64decode(
-    module.eks.cluster_ca_certificate
-  )
-
-  token = data.aws_eks_cluster_auth.this.token
-
-  load_config_file = false
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
 }

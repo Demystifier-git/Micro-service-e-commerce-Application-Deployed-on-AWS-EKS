dd67185f360c1cc46dc7b37cae5f3fa946438e02
devops4sure <devops4sure@gmail.com>
2026-08-16 14:34:15 -0700

    added external-dns

---
+++ terraform/modules/external-dns/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl" # must match your root
+      version = "~> 1.14"
+    }
+  }
+}
--- terraform/node-group.tf
+++ terraform/node-group.tf
@@ -12,15 +12,6 @@ resource "aws_security_group" "node_sg" {
   # - AWS VPC endpoints (ECR, S3, STS, etc.)
 
 
-  ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb_sg.id] # replace with your ALB SG
-    description     = "Allow ALB to reach ArgoCD pods"
-  }
-
-
   # Loki logs
   egress {
     from_port       = 3100

f863115769c1cd6dc854aca49c1c74b335e2e9f6
devops4sure <devops4sure@gmail.com>
2026-08-31 00:46:33 -0700

    backend.tf updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "prod-terraform-state-245361884126 "
+    bucket         = "prod-terraform-state-245361884126"
     key            = "prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "prod-terraform-locks"
